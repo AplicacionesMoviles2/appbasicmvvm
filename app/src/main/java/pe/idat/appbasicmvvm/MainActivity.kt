3426bf1baeb835b40b068318d27858d2e3b64d12
AplicacionesMoviles2 <dam2.idat2024@gmail.com>
2024-07-10 19:54:05 -0500

    Definición de rutas en el MainActivity

--- app/src/main/java/pe/idat/appbasicmvvm/MainActivity.kt
+++ app/src/main/java/pe/idat/appbasicmvvm/MainActivity.kt
@@ -11,9 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import pe.idat.appbasicmvvm.auth.AuthViewModel
 import pe.idat.appbasicmvvm.auth.authScreen
+import pe.idat.appbasicmvvm.home.homeScreen
+import pe.idat.appbasicmvvm.routes.AppRoutes
 import pe.idat.appbasicmvvm.ui.theme.AppbasicmvvmTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +27,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppbasicmvvmTheme {
                 val navigation = rememberNavController()
-
-                authScreen(AuthViewModel())
+                NavHost(navController = navigation,
+                    startDestination = AppRoutes.loginScreen.path,
+                    builder = {
+                        composable(AppRoutes.loginScreen.path){
+                            authScreen(AuthViewModel())
+                        }
+                        composable(AppRoutes.homeScreen.path){
+                            homeScreen()
+                        }
+                    })
             }
         }
     }
